6aa7f238787732a05c406032ea2e6c1e24b9b540
Habibulloh3 <saidhonovnodirhon@gmail.com>
2024-12-28 18:28:32 +0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily:"Concert",
+        elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(textStyle: TextStyle(fontFamily: "Concert")))
+      ),
+      home:MyApp(),
+    );
+  }
+}
+
+
+
+
